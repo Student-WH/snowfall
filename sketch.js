52464d07c611a2e1e3b01878831fac3c2e9e43c9
Student-WH <73741780+Student-WH@users.noreply.github.com>
2021-05-08 07:30:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function preload(){
+  bg = loadImage("snow1.jpg")
+}
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(bg);  
+  drawSprites();
+}
